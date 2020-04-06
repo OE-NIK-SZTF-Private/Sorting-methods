3bf19c1d65b1e22bb83aabcd654d57366db68336
hchriss1013 <34167101+hchriss1013@users.noreply.github.com>
2020-04-06 13:34:53 +0200

    Update MinimalSorting method
    Update v1.1.1
     - Updated MinimalSort optional parameters name and definition's form;
     - Updtated the description of mehod;
     - Fixed a possible problem for wrong indexing while using method (IndexOutOFRangeException)

--- Sorting.cs
+++ Sorting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,32 +11,39 @@ namespace Sokminden_egybe
     {
         #region MinimalSorting
         /// <summary>
-        /// Sort a List or an array in ascendant order.
-        /// (Optional) Indexing starts from zero. Start and last index included in sorting.
+        /// Sort a List or an array in ascendant order. Indexing start from zero and tend to run until last index.
+        /// (Optional): Sorting can be run at a specific range of indexes.
         /// </summary>
         /// <param name="array">Integer array.</param>
-        /// <param name="startRange">(Optional) The first index of the range where the sorting will run.</param>
-        /// <param name="endRange">(Optional) The last index of the range where the sorting will run.</param>
-        public static void MinimalSort(int[] array, int startRange = 0, int endRange = 0)
+        /// <param name="startOfRange">(Optional) The first index of the range where the sorting will run.</param>
+        /// <param name="endOfRange">(Optional) The last index of the range where the sorting will run.</param>
+        public static void MinimalSort(int[] array, [Optional, DefaultParameterValue(0)]int startOfRange, [Optional, DefaultParameterValue(0)]int endOfRange)
         {
-            if (endRange == 0)
+            if (endOfRange == 0)
             {
-                endRange = array.Length;
+                endOfRange = array.Length;
             }
-            for (int i = startRange; i < endRange - 1; i++)
+            try
             {
-                int min = i;
-                for (int j = i + 1; j < endRange; j++)
+                for (int i = startOfRange; i < endOfRange - 1; i++)
                 {
-                    if (array[min] > array[j])
+                    int min = i;
+                    for (int j = i + 1; j < endOfRange; j++)
                     {
-                        min = j;
+                        if (array[min] > array[j])
+                        {
+                            min = j;
+                        }
                     }
+                    int seged = array[i];
+                    array[i] = array[min];
+                    array[min] = seged;
                 }
-                int seged = array[i];
-                array[i] = array[min];
-                array[min] = seged;
             }
+            catch (IndexOutOfRangeException)
+            {
+                throw;
+            } 
         }
 
         /// <summary>
@@ -45,25 +53,32 @@ namespace Sokminden_egybe
         /// <param name="array">Double array.</param>
         /// <param name="startRange">(Optional) The first index of the range where the sorting will run.</param>
         /// <param name="endRange">(Optional) The last index of the range where the sorting will run.</param>
-        public static void MinimalSort(double[] array, int startRange = 0, int endRange = 0)
+        public static void MinimalSort(double[] array, [Optional, DefaultParameterValue(0)]int startRange, [Optional, DefaultParameterValue(0)]int endRange)
         {
             if (endRange == 0)
             {
                 endRange = array.Length;
             }
-            for (int i = startRange; i < endRange - 1; i++)
+            try
             {
-                int min = i;
-                for (int j = i + 1; j < endRange; j++)
+                for (int i = startRange; i < endRange - 1; i++)
                 {
-                    if (array[min] > array[j])
+                    int min = i;
+                    for (int j = i + 1; j < endRange; j++)
                     {
-                        min = j;
+                        if (array[min] > array[j])
+                        {
+                            min = j;
+                        }
                     }
+                    double seged = array[i];
+                    array[i] = array[min];
+                    array[min] = seged;
                 }
-                double seged = array[i];
-                array[i] = array[min];
-                array[min] = seged;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw;
             }
         }
 
@@ -74,25 +89,32 @@ namespace Sokminden_egybe
         /// <param name="array">Float array.</param>
         /// <param name="startRange">(Optional) The first index of the range where the sorting will run.</param>
         /// <param name="endRange">(Optional) The last index of the range where the sorting will run.</param>
-        public static void MinimalSort(float[] array, int startRange = 0, int endRange = 0)
+        public static void MinimalSort(float[] array, [Optional, DefaultParameterValue(0)]int startRange, [Optional, DefaultParameterValue(0)]int endRange)
         {
             if (endRange == 0)
             {
                 endRange = array.Length;
             }
-            for (int i = startRange; i < endRange - 1; i++)
+            try
             {
-                int min = i;
-                for (int j = i + 1; j < endRange; j++)
+                for (int i = startRange; i < endRange - 1; i++)
                 {
-                    if (array[min] > array[j])
+                    int min = i;
+                    for (int j = i + 1; j < endRange; j++)
                     {
-                        min = j;
+                        if (array[min] > array[j])
+                        {
+                            min = j;
+                        }
                     }
+                    float seged = array[i];
+                    array[i] = array[min];
+                    array[min] = seged;
                 }
-                float seged = array[i];
-                array[i] = array[min];
-                array[min] = seged;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw;
             }
         }
 
@@ -103,25 +125,32 @@ namespace Sokminden_egybe
         /// <param name="array">Character array.</param>
         /// <param name="startRange">(Optional) The first index of the range where the sorting will run.</param>
         /// <param name="endRange">(Optional) The last index of the range where the sorting will run.</param>
-        public static void MinimalSort(char[] array, int startRange = 0, int endRange = 0)
+        public static void MinimalSort(char[] array, [Optional, DefaultParameterValue(0)]int startRange, [Optional, DefaultParameterValue(0)]int endRange)
         {
             if (endRange == 0)
             {
                 endRange = array.Length;
             }
-            for (int i = startRange; i < endRange - 1; i++)
+            try
             {
-                int min = i;
-                for (int j = i + 1; j < endRange; j++)
+                for (int i = startRange; i < endRange - 1; i++)
                 {
-                    if (array[min] > array[j])
+                    int min = i;
+                    for (int j = i + 1; j < endRange; j++)
                     {
-                        min = j;
+                        if (array[min] > array[j])
+                        {
+                            min = j;
+                        }
                     }
+                    char seged = array[i];
+                    array[i] = array[min];
+                    array[min] = seged;
                 }
-                char seged = array[i];
-                array[i] = array[min];
-                array[min] = seged;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw;
             }
         }
 
@@ -132,25 +161,32 @@ namespace Sokminden_egybe
         /// <param name="array">Integer List.</param>
         /// <param name="startRange">(Optional) The first index of the range where the sorting will run.</param>
         /// <param name="endRange">(Optional) The last index of the range where the sorting will run.</param>
-        public static void MinimalSort(List<int> list , int startRange = 0, int endRange = 0)
+        public static void MinimalSort(List<int> list, [Optional, DefaultParameterValue(0)]int startRange, [Optional, DefaultParameterValue(0)]int endRange)
         {
             if (endRange == 0)
             {
                 endRange = list.Count;
             }
-            for (int i = startRange; i < endRange - 1; i++)
+            try
             {
-                int min = i;
-                for (int j = i + 1; j < endRange; j++)
+                for (int i = startRange; i < endRange - 1; i++)
                 {
-                    if (list[min] > list[j])
+                    int min = i;
+                    for (int j = i + 1; j < endRange; j++)
                     {
-                        min = j;
+                        if (list[min] > list[j])
+                        {
+                            min = j;
+                        }
                     }
+                    int seged = list[i];
+                    list[i] = list[min];
+                    list[min] = seged;
                 }
-                int seged = list[i];
-                list[i] = list[min];
-                list[min] = seged;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw;
             }
         }
 
@@ -161,25 +197,32 @@ namespace Sokminden_egybe
         /// <param name="array">Double List.</param>
         /// <param name="startRange">(Optional) The first index of the range where the sorting will run.</param>
         /// <param name="endRange">(Optional) The last index of the range where the sorting will run.</param>
-        public static void MinimalSort(List<double> list, int startRange = 0, int endRange = 0)
+        public static void MinimalSort(List<double> list, [Optional, DefaultParameterValue(0)]int startRange, [Optional, DefaultParameterValue(0)]int endRange)
         {
             if (endRange == 0)
             {
                 endRange = list.Count;
             }
-            for (int i = startRange; i < endRange - 1; i++)
+            try
             {
-                int min = i;
-                for (int j = i + 1; j < endRange; j++)
+                for (int i = startRange; i < endRange - 1; i++)
                 {
-                    if (list[min] > list[j])
+                    int min = i;
+                    for (int j = i + 1; j < endRange; j++)
                     {
-                        min = j;
+                        if (list[min] > list[j])
+                        {
+                            min = j;
+                        }
                     }
+                    double seged = list[i];
+                    list[i] = list[min];
+                    list[min] = seged;
                 }
-                double seged = list[i];
-                list[i] = list[min];
-                list[min] = seged;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw;
             }
         }
 
@@ -190,25 +233,32 @@ namespace Sokminden_egybe
         /// <param name="array">Float List.</param>
         /// <param name="startRange">(Optional) The first index of the range where the sorting will run.</param>
         /// <param name="endRange">(Optional) The last index of the range where the sorting will run.</param>
-        public static void MinimalSort(List<float> list, int startRange = 0, int endRange = 0)
+        public static void MinimalSort(List<float> list, [Optional, DefaultParameterValue(0)]int startRange, [Optional, DefaultParameterValue(0)]int endRange)
         {
             if (endRange == 0)
             {
                 endRange = list.Count;
             }
-            for (int i = startRange; i < endRange - 1; i++)
+            try
             {
-                int min = i;
-                for (int j = i + 1; j < endRange; j++)
+                for (int i = startRange; i < endRange - 1; i++)
                 {
-                    if (list[min] > list[j])
+                    int min = i;
+                    for (int j = i + 1; j < endRange; j++)
                     {
-                        min = j;
+                        if (list[min] > list[j])
+                        {
+                            min = j;
+                        }
                     }
+                    float seged = list[i];
+                    list[i] = list[min];
+                    list[min] = seged;
                 }
-                float seged = list[i];
-                list[i] = list[min];
-                list[min] = seged;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw;
             }
         }
 
@@ -219,27 +269,38 @@ namespace Sokminden_egybe
         /// <param name="array">Character List.</param>
         /// <param name="startRange">(Optional) The first index of the range where the sorting will run.</param>
         /// <param name="endRange">(Optional) The last index of the range where the sorting will run.</param>
-        public static void MinimalSort(List<char> list, int startRange = 0, int endRange = 0)
+        public static void MinimalSort(List<char> list, [Optional, DefaultParameterValue(0)]int startRange, [Optional, DefaultParameterValue(0)]int endRange)
         {
             if (endRange == 0)
             {
                 endRange = list.Count;
             }
-            for (int i = startRange; i < endRange - 1; i++)
+            try
             {
-                int min = i;
-                for (int j = i + 1; j < endRange; j++)
+                for (int i = startRange; i < endRange - 1; i++)
                 {
-                    if (list[min] > list[j])
+                    int min = i;
+                    for (int j = i + 1; j < endRange; j++)
                     {
-                        min = j;
+                        if (list[min] > list[j])
+                        {
+                            min = j;
+                        }
                     }
+                    char seged = list[i];
+                    list[i] = list[min];
+                    list[min] = seged;
                 }
-                char seged = list[i];
-                list[i] = list[min];
-                list[min] = seged;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw;
             }
         }
+        #endregion
+
+        #region BubbelSort
+
         #endregion
     }
 }
