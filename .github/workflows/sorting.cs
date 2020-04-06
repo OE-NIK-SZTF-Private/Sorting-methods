89d3856e9bc7a5a19499d7d04a2f035544a08425
hchriss1013 <34167101+hchriss1013@users.noreply.github.com>
2020-04-06 11:51:20 +0200

    Create sorting.cs

---
+++ .github/workflows/sorting.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sokminden_egybe
+{
+    static class Sorting
+    {
+
+        public static void MinimalSort(int[] array)
+        {
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if(array[min]>array[j])
+                    {
+                        min = j;
+                    }
+                }
+                int seged = array[i];
+                array[i] = array[min];
+                array[min] = seged;
+            }
+        }
+
+        public static void MinimalSort(float[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[min] > array[j])
+                    {
+                        min = j;
+                    }
+                }
+                float seged = array[i];
+                array[i] = array[min];
+                array[min] = seged;
+            }
+        }
+
+        public static void MinimalSort(double[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[min] > array[j])
+                    {
+                        min = j;
+                    }
+                }
+                double seged = array[i];
+                array[i] = array[min];
+                array[min] = seged;
+            }
+        }
+
+        public static void MinimalSort(char[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[min] > array[j])
+                    {
+                        min = j;
+                    }
+                }
+                char seged = array[i];
+                array[i] = array[min];
+                array[min] = seged;
+            }
+        }
+
+        public static void MinimalSort(List<int> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[min] > list[j])
+                    {
+                        min = j;
+                    }
+                }
+                int seged = list[i];
+                list[i] = list[min];
+                list[min] = seged;
+            }
+        }
+
+        public static void MinimalSort(List<float> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[min] > list[j])
+                    {
+                        min = j;
+                    }
+                }
+                float seged = list[i];
+                list[i] = list[min];
+                list[min] = seged;
+            }
+        }
+
+        public static void MinimalSort(List<double> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[min] > list[j])
+                    {
+                        min = j;
+                    }
+                }
+                double seged = list[i];
+                list[i] = list[min];
+                list[min] = seged;
+            }
+        }
+
+        public static void MinimalSort(List<char> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[min] > list[j])
+                    {
+                        min = j;
+                    }
+                }
+                char seged = list[i];
+                list[i] = list[min];
+                list[min] = seged;
+            }
+        }
+
+    }
+}
